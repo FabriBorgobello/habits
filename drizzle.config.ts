ac4bd6067f3ae430fd2aadf8064f571c2fca9ea1
Fabricio Borgobello <fborgobello@gmail.com>
2026-01-17 17:17:41 +0100

    Format

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,14 @@
-import { config } from 'dotenv'
-import { defineConfig } from 'drizzle-kit'
+import { env } from "@/env";
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
 
-config({ path: ['.env.local', '.env'] })
+config({ path: [".env.local", ".env"] });
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
-  dialect: 'postgresql',
+  out: "./drizzle",
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: env.DATABASE_URL,
   },
-})
+});
