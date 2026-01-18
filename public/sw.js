b5f17f14649eae4aa85eb6882af0932726621ade
Fabricio Borgobello <fborgobello@gmail.com>
2026-01-18 14:33:36 +0100

    Rename static assets

--- public/sw.js
+++ public/sw.js
@@ -3,12 +3,12 @@ const CACHE_NAME = "habits-v1";
 const STATIC_ASSETS = [
   "/",
   "/manifest.json",
-  "/favicon.ico",
-  "/favicon-16x16.png",
-  "/favicon-32x32.png",
-  "/apple-touch-icon.png",
-  "/android-chrome-192x192.png",
-  "/android-chrome-512x512.png",
+  "/favicon.ico?v=1",
+  "/favicon-16x16.png?v=1",
+  "/favicon-32x32.png?v=1",
+  "/apple-touch-icon.png?v=1",
+  "/android-chrome-192x192.png?v=1",
+  "/android-chrome-512x512.png?v=1",
 ];
 
 // Install: cache static assets
