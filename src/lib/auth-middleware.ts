64648f47a5133c72fe126ef21fdff4fb67789e27
Fabricio Borgobello <fborgobello@gmail.com>
2026-01-18 14:21:36 +0100

    Adjust paths

--- src/lib/auth-middleware.ts
+++ src/lib/auth-middleware.ts
@@ -6,10 +6,6 @@ import { auth } from "@/lib/auth";
 export const authMiddleware = createMiddleware().server(async ({ next }) => {
   const headers = getRequestHeaders();
   const session = await auth.api.getSession({ headers });
-
-  if (!session) {
-    throw redirect({ to: "/sign-in" });
-  }
-
+  if (!session) throw redirect({ to: "/" });
   return await next();
 });
